a3f680b5d40da72e99793bed51b2eb3589fa71cc
admin <ahrier@163.com>
2017-07-11 19:56:17 +0800

    ceshi

--- admin/ctrl/indexCtrl.php
+++ admin/ctrl/indexCtrl.php
@@ -2,6 +2,7 @@
 namespace admin\ctrl;
 class indexCtrl extends \core\myProject{
     public function index(){
+        echo 111;
         $model = new \admin\model\ceshiModel();
         $data = $model->selectAll();
         $this->assign('data',$data);
